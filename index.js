3d49bbc036ee2e2e403c35c9937265c9ab29da13
Nicolas Herbreteau <nicolasherbreteau@Nicolass-MacBook-Pro.local>
2022-11-09 18:38:22 +0000

    bug fixed

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const menuContainer = document.querySelector('#mobileMenu')
 const closeMenu = document.querySelector('#close')
 
 menuButton.addEventListener('click', () => {
-    menuContainer.style.zIndex = '1'
     menuContainer.style.display = 'flex'
+    menuButton.classList.toggle("active");
+    body.classList.toggle("blur");
 })
 
 closeMenu.addEventListener('click', () => {
